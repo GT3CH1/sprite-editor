a9ed2c09936b01fff4379a276ccf87f71601a887
Gavin Pease <gavinpease@gmail.com>
2021-11-10 11:07:40 -0700

    Add basic scroll area, slider
    Need to add more buttons & other features

--- SpriteEditor/spriteeditorvc.h
+++ SpriteEditor/spriteeditorvc.h
@@ -2,6 +2,12 @@
 #define SPRITEEDITORVC_H
 
 #include <QMainWindow>
+#include <QScrollArea>
+#include <QPushButton>
+#include <QSlider>
+#include <QLabel>
+
+// TODO(gcpease): Add QRenderArea include
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class SpriteEditorVC; }
@@ -12,8 +18,12 @@ class SpriteEditorVC : public QMainWindow
 	Q_OBJECT
 
 public:
+    // TODO(gcpease): Add all the buttons for tools
+    QPushButton *tool1;
+
 	SpriteEditorVC(QWidget *parent = nullptr);
 	~SpriteEditorVC();
+
 
 private:
 	Ui::SpriteEditorVC *ui;
