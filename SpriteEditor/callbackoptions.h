4a569ea6bc9ba039606c2169c71dfda51e65c512
werignac <77596308+werignac-utah@users.noreply.github.com>
2021-11-10 11:03:46 -0700

    callback options 3/3

--- SpriteEditor/callbackoptions.h
+++ SpriteEditor/callbackoptions.h
@@ -3,13 +3,33 @@
 
 #include <QColor>
 
+///
+/// \brief The CallbackOptions struct is the means through which tools can affect the model.
+///
 struct CallbackOptions
 {
 public:
 	CallbackOptions(void (*_setPixelColors)(QColor**, unsigned int width, unsigned int height, unsigned int xCoord, unsigned int yCoord),
 					void (*_setPixelColor)(QColor, unsigned int xCoord, unsigned int yCoord));
 	//https://stackoverflow.com/questions/10003270/gcc-array-type-has-incomplete-element-type
+
+
+	///
+	/// \param QColor** a rectangular, 2D array of colors.
+	/// \param width the 2D array of colors.
+	/// \param height of the 2D array of colors.
+	///	\param xCoord the displacement of the pixels to be set in the x direction
+	///		(x coordinate of upper left pixel in 2D array).
+	/// \param yCoord the  displacement of the pixels to be set in the y direction
+	///		(y coordinate of upper left pixel in 2D array).
+	///
 	void (*setPixelColors)(QColor**, uint width, uint height, unsigned int xCoord, unsigned int yCoord);
+
+	///
+	/// \param QColor the color to set the given pixel.
+	/// \param width the x coordinate of the pixel.
+	/// \param height the y coordinate of the pixel.
+	///
 	void (*setPixelColor)(QColor, unsigned int xCoord, unsigned int yCoord);
 };
 
