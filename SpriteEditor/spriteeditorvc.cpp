3fdb2b7bdcda89f8d170fc3f30972d344f4321bc
JVielstich <jonathan.vielstich@utah.edu>
2021-11-15 14:47:29 -0700

    [2/?] Implemented Save and Open File dialogs

--- SpriteEditor/spriteeditorvc.cpp
+++ SpriteEditor/spriteeditorvc.cpp
@@ -28,3 +28,42 @@ void SpriteEditorVC::on_fpsSlider_valueChanged(int value)
     ui->fpsLabel->setText(QString::number(value));
 }
 
+/**
+ * @brief Launches the "Save File" dialog window and prompts the user
+ * to choose a name and location for saving the active sprite. The file
+ * path and name will be passed to the model.
+ */
+void SpriteEditorVC::savePressed()
+{
+	path = QFileDialog::getSaveFileName(this, tr("Save File"), path, tr(FILE_FILTER));
+	std::string pathAsString(path.toStdString());
+
+	auto i(pathAsString.find_last_of("/"));
+
+	if (i != std::string::npos)
+	{
+		path = QString::fromStdString(pathAsString.substr(0, i + 1));
+		std::string name(pathAsString.substr(i + 1));
+		emit save(path.toStdString(), name);
+	}
+}
+
+/**
+ * @brief Launches the "Open File" dialog window and prompts the user
+ * to choose a file to open and replace the active sprite. The file
+ * path and name will be passed to the model.
+ */
+void SpriteEditorVC::loadPressed()
+{
+	path = QFileDialog::getOpenFileName(this, tr("Open File"), path, tr(FILE_FILTER));
+	std::string pathAsString(path.toStdString());
+
+	auto i(pathAsString.find_last_of("/"));
+
+	if (i != std::string::npos)
+	{
+		path = QString::fromStdString(pathAsString.substr(0, i + 1));
+		std::string name(pathAsString.substr(i + 1));
+		emit load(path.toStdString(), name);
+	}
+}
